5b9427c4fcc6333fc371ed727adba9588bf28704
mhd3v <mamir3012@gmail.com>
2020-06-06 12:52:39 +0500

    Bump up version number and fix minor margin issue

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,7 +1,7 @@
 import { environment } from '../environments/environment';
 
 export const constants = {
-    version: '0.12',
+    version: '0.14',
     apiUrl: environment.production ? 'https://db-polling.herokuapp.com' : 'http://localhost:3000',
     answerTypes: {
         binary: 'binary',
