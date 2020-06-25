07ea125c334f68cfc5908e6a8093341c8eab8326
mhd3v <mamir3012@gmail.com>
2020-06-26 01:20:59 +0500

    Update sharing url structure

--- src/app/shared/utils/utility.ts
+++ src/app/shared/utils/utility.ts
@@ -2,6 +2,6 @@ import { constants } from "src/app/app.constants";
 
 export class Utility {
   public static getPollUrl(poll) {
-    return window.location.origin + `/p?id=${poll.shortId}`;
+    return window.location.origin + `/p/${poll.shortId}`;
   }
 }
