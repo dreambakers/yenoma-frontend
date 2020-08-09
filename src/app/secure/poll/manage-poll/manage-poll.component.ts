79165501d18f1750e00f4e3d1d39a9d6a7fbdfe4
mhd3v <mamir3012@gmail.com>
2020-08-09 19:48:59 +0500

    Remove dependency on responses in manage poll component

--- src/app/secure/poll/manage-poll/manage-poll.component.ts
+++ src/app/secure/poll/manage-poll/manage-poll.component.ts
@@ -32,7 +32,6 @@ export class ManagePollComponent implements OnInit, OnDestroy {
   user;
   poll: Poll;
   pollCopy;
-  responses;
   answerMap: any;
   hide = true;
   preview = false;
@@ -71,14 +70,13 @@ export class ManagePollComponent implements OnInit, OnDestroy {
           (res: any) => {
             if (res.success) {
               this.poll = res.poll;
-              this.responses = res.responses;
               this.showPassword = !!this.poll.password;
               this.pollCopy = JSON.stringify(this.poll);
               this.poll.questions.forEach((q, i) => { this.collapsed[i] = true; });
               this.updateMobileNavbar();
-              if (this.responses.length) {
+              if (this.poll.responses >= 1) {
                 this.emitterService.emit(constants.emitterKeys.changeNavbarTitle, {
-                  extra: ` (${this.responses.length})`
+                  extra: ` (${this.poll.responses})`
                 });
               }
             } else {
@@ -396,7 +394,7 @@ export class ManagePollComponent implements OnInit, OnDestroy {
 
   get shouldDisable() {
     if (this.isEditing) {
-      return this.responses.length > 0;
+      return this.poll.responses > 0;
     } else {
       return false;
     }
